1abc0a97f473431de87e8fb2030f3ec775a18d31
aniketocto <aniketocto@gmail.com>
2025-12-22 22:12:21 +0530

    added cors

--- app/main.py
+++ app/main.py
@@ -4,10 +4,23 @@ from fastapi.staticfiles import StaticFiles
 from app.api.predict import router as predict_router
 from app.core.config import OUTPUT_DIR
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 app = FastAPI(
     title = "Indian Flower Recognition API",
     description = "An API to recognize Indian flowers using a pre-trained MobileNet model.",
     version = "1.0.0"
+)
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Serve frontend
@@ -27,3 +40,7 @@ def root():
         "status": "OK",
         "message" : "Indian Flower Recognition API is running."
     }
+
+@app.get("/health")
+def health():
+    return {"status": "ok", "model": "loaded"}
